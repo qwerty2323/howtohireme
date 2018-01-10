50d9b643e5cdd423db05f288920cd90cc760346b
Ivan Shelomentsev <Shelomentsev.Ivan@gmail.com>
2018-01-10 23:56:52 +0500

    HTHM-1 : Added total sum to order

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,11 @@
 class Order < ApplicationRecord
   validates :cart_id, presence: true, numericality: { only_integer: true }
   validates :items, presence: true, length: {  minimum: 1 }
+  def items
+    @items ||= []
+  end
+
+  def total
+    @items.map(&:price).inject(0, &:+)
+  end
 end
--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -12,4 +12,14 @@ RSpec.describe Order, type: :model do
     @order.cart_id = 1
     expect(@order.valid?).to be false
   end
+
+  it 'sums item prices' do
+    @order = Order.new
+    @item = Item.new
+    @item.price = 2
+    5.times { @order.items << @item }
+    expect(@order.items.length).to eq(5)
+    expect(@order.items[0].price).to eq(2)
+    expect(@order.total).to eq(10)
+  end
 end
